6bd048df3a75e243af6f3d1802c516b1735cfc24
killvung <killvung@gmail.com>
2021-05-22 19:55:48 -0500

    Log for testing

--- src/strava/strava.module.ts
+++ src/strava/strava.module.ts
@@ -1,4 +1,6 @@
-import { Module } from '@nestjs/common';
+import { Module, Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
 import { StravaController } from './strava.controller';
 import { StravaService } from './strava.service';
 
@@ -7,4 +9,10 @@ import { StravaService } from './strava.service';
     controllers: [StravaController],
     providers: [StravaService],
 })
-export class StravaModule { }
+export class StravaModule {
+    constructor(private configService: ConfigService) {
+        Logger.log(`APP__APP_ENV: ${configService.get("APP__APP_ENV")}`)
+        Logger.log(`FOO: ${configService.get("FOO")}`)
+        Logger.log(`STAGE: ${configService.get("STAGE")}`)
+    }
+}
